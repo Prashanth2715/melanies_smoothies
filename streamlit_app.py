f0edf98ad7f4c67aa7baedb709c17d65215da2f5
Haranagiri Prashanth <42687791+Prashanth2715@users.noreply.github.com>
2024-07-24 10:35:04 -0400

    Update streamlit_app.py
    fruit vice api call changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -31,7 +32,10 @@ if ingredients_list and name_on_order:
     if submit_order:
         session.sql(insert_stmt).collect()
         st.success('Smoothie Ordered '+name_on_order,icon="✅")
-        
+
+#displaying fruitvice data
+fruitvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+fv_df=st.dataframe(fruitvice_response.json(),use_container_width=true)
         
 
 
